730fea8def9b3b1fde5f767896cc4058c767f08d
hsrazna <anzar.terek@gmail.com>
2016-11-28 15:05:24 +0300

    cxvx

---
+++ design.php
@@ -0,0 +1,266 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+  <!-- saved from url=(0014)about:internet -->
+  <meta charset="UTF-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title> </title>
+
+  <!-- icons -->
+  <link href="favicon.ico" rel="shortcut icon">
+
+  <!-- css + javascript -->
+  <link rel="stylesheet" href="css/main.css" media="all">
+
+  <!--[if lt IE 9]>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+
+</head>
+<body>
+
+  <header role="banner">
+
+    <div class="header--logo">
+      <img src="img/logo.png" alt="" title="">
+    </div><!-- /logo -->
+
+    <nav class="header--nav" role="navigation">
+      <ul class="headnav">
+        <li><a href="/">старинный<span>кирпич</span></a></li>
+        <li><a href="/plitkaloft.php">плитка<span>из кирпича</span></a></li>
+        <li><a href="/design.php">дизайнерские<span>решения (Loft)</span></a></li>
+        <li><a href="/works.php">наши<span>работы</span></a></li>
+      </ul>
+    </nav><!-- /nav -->
+
+    <a class="header--phone" href="tel:88002001183">8 800 200 11 83<span>звонок по России бесплатно</span></a>
+
+  </header><!-- /header -->
+
+  <div class="mainbag">
+
+
+    <div class="mainview design-first arrow-bottom">
+      <div class="vs-center-wrap arrow-top">
+        <div class="vs-center">
+
+          <h1 class="design--title arrow-top">дизайн-проекты в стиле <span>LOFT</span></h1>
+          <div class="fogged">
+
+            <div class="design--item">
+              <img src="img/design--item-1.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-2.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-3.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-4.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-5.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-6.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-7.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-8.jpg" alt="">
+              <a href="#" class="btn btn-yellow">оставить заявку</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-9.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+
+            <a href="#" class="btn btn-red">скачать полный каталог</a>
+          </div><!-- /.fogged -->
+
+        </div><!-- vs-center -->
+      </div><!-- vs-center-wrap -->
+    </div><!-- mainview -->
+
+    <div class="mainview design-second arrow-bottom">
+      <div class="vs-center-wrap arrow-top">
+        <div class="vs-center">
+
+          <h1 class="design--title arrow-top">дизайн-проекты в стиле <span>LOFT</span></h1>
+          <div class="fogged">
+
+            <div class="design--item">
+              <img src="img/design--item-10.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-11.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-12.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-13.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-14.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-15.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-16.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-17.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-18.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+
+            <a href="#" class="btn btn-red">скачать полный каталог</a>
+          </div><!-- /.fogged -->
+
+        </div><!-- vs-center -->
+      </div><!-- vs-center-wrap -->
+    </div><!-- mainview -->
+
+    <div class="mainview design-third arrow-bottom">
+      <div class="vs-center-wrap arrow-top">
+        <div class="vs-center">
+
+          <h1 class="design--title arrow-top">дизайн-проекты в стиле <span>LOFT</span></h1>
+          <div class="fogged">
+
+            <div class="design--item">
+              <img src="img/design--item-19.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-20.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-21.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-22.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-23.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-24.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-25.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-26.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-27.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+
+            <a href="#" class="btn btn-red">скачать полный каталог</a>
+          </div><!-- /.fogged -->
+
+        </div><!-- vs-center -->
+      </div><!-- vs-center-wrap -->
+    </div><!-- mainview -->
+
+    <div class="mainview design-fourth arrow-bottom">
+      <div class="vs-center-wrap arrow-top">
+        <div class="vs-center">
+
+          <div class="fogged">
+            <h1 class="design-fourth--title">хотите увидеть еще <span>150</span></h1>
+            <h2 class="design-fourth--subtitle">дизайнерских решений</h2>
+            <h3 class="design-fourth--thirdtitle">в стиле <span>LOFT</span>?</h3>
+            <h5 class="design-fourth--description">скачайте полный каталог дизайн-проектов в стиле <span>LOFT</span> из старинного кирпича и плитки прямо сейчас</h5>
+            <a href="#" class="btn btn-red">скачать каталог дизайн-проектов</a>
+            <h4 class="design-fourth--fourthtitle">остались вопросы? <span>задайте их менеджеру</span></h4>
+          </div><!-- /.fogged -->
+
+        </div><!-- vs-center -->
+      </div><!-- vs-center-wrap -->
+    </div><!-- mainview -->
+
+  </div><!-- mainbag -->
+
+  <div class="modalbg modal-ancient-offer">
+    <div class="modal-c">
+      <h5>получите полный каталог</h5>
+      <h6>заполните поля ниже и мы вышлем вам полный каталог старинного кирпича</h6>
+      <form method="post">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="phone" data-inputmask="'alias': 'phone'" placeholder="номер телефона">
+        <button class="btn btn-red">получить полный каталог</button>
+      </form>
+      <span class="form-sended">ваша заяка принята<span>мы свяжемся с вами в ближайшее время</span></span>
+      <span class="close"></span>
+    </div><!-- /.modal-c -->
+  </div><!-- /.modalbg -->
+
+  <div class="modalbg modal-ancient-sms">
+    <div class="modal-c">
+      <h6>введите данные в форму ниже мы свяжемся с вами и обсудим детли вашего проекта </h6>
+      <form method="post">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="phone" data-inputmask="'alias': 'phone'" placeholder="номер телефона">
+        <button class="btn btn-red">получить полный каталог</button>
+      </form>
+      <span class="form-sended">ваша заяка принята<span>мы свяжемся с вами в ближайшее время</span></span>
+      <span class="close"></span>
+    </div><!-- /.modal-c -->
+  </div><!-- /.modalbg -->
+
+  <div class="modalbg">
+    <div class="modal-c">
+      <h5>получите полный каталог</h5>
+      <h6>заполните поля ниже и мы вышлем вам полный каталог старинного кирпича</h6>
+      <form action="">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="phone" data-inputmask="'alias': 'phone'" placeholder="номер телефона">
+        <button class="btn btn-red">получить полный каталог</button>
+      </form>
+      <span class="close"></span>
+    </div><!-- /.modal-c -->
+  </div><!-- /.modalbg -->
+
+
+  <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+  <script type="text/javascript" src="js/scripts.js"></script>
+
+</body>
+</html>
---
+++ works.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+  <!-- saved from url=(0014)about:internet -->
+  <meta charset="UTF-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title> </title>
+
+  <!-- icons -->
+  <link href="favicon.ico" rel="shortcut icon">
+
+  <!-- css + javascript -->
+  <link rel="stylesheet" href="css/main.css" media="all">
+
+  <!--[if lt IE 9]>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+
+</head>
+<body>
+
+  <header role="banner">
+
+    <div class="header--logo">
+      <img src="img/logo.png" alt="" title="">
+    </div><!-- /logo -->
+
+    <nav class="header--nav" role="navigation">
+      <ul class="headnav">
+        <li><a href="/">старинный<span>кирпич</span></a></li>
+        <li><a href="/plitkaloft.php">плитка<span>из кирпича</span></a></li>
+        <li><a href="/design.php">дизайнерские<span>решения (Loft)</span></a></li>
+        <li><a href="/works.php">наши<span>работы</span></a></li>
+      </ul>
+    </nav><!-- /nav -->
+
+    <a class="header--phone" href="tel:88002001183">8 800 200 11 83<span>звонок по России бесплатно</span></a>
+
+  </header><!-- /header -->
+
+  <div class="mainbag">
+
+
+    <div class="mainview design-first arrow-bottom">
+      <div class="vs-center-wrap arrow-top">
+        <div class="vs-center">
+
+          <h1 class="design--title arrow-top">наши работы</h1>
+          <div class="fogged">
+
+            <div class="design--item">
+              <img src="img/design--item-31.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-32.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-33.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-34.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-35.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-36.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-37.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-38.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-39.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+          </div><!-- /.fogged -->
+
+        </div><!-- vs-center -->
+      </div><!-- vs-center-wrap -->
+    </div><!-- mainview -->
+
+    <div class="mainview design-second arrow-bottom">
+      <div class="vs-center-wrap arrow-top">
+        <div class="vs-center">
+
+          <h1 class="design--title arrow-top">наши работы</h1>
+          <div class="fogged">
+
+            <div class="design--item">
+              <img src="img/design--item-41.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-42.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-43.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-44.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-45.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-46.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-47.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-48.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-49.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+          </div><!-- /.fogged -->
+
+        </div><!-- vs-center -->
+      </div><!-- vs-center-wrap -->
+    </div><!-- mainview -->
+
+    <div class="mainview design-third arrow-bottom">
+      <div class="vs-center-wrap arrow-top">
+        <div class="vs-center">
+
+          <h1 class="design--title arrow-top">наши работы</h1>
+          <div class="fogged">
+
+            <div class="design--item">
+              <img src="img/design--item-51.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-52.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-53.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-54.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-55.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-56.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-57.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-58.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+            <div class="design--item">
+              <img src="img/design--item-59.jpg" alt="">
+              <a href="#" class="btn btn-yellow">хочу подобный проект</a>
+            </div><!-- /.design--item -->
+          </div><!-- /.fogged -->
+
+        </div><!-- vs-center -->
+      </div><!-- vs-center-wrap -->
+    </div><!-- mainview -->
+
+    <div class="mainview design-five arrow-bottom">
+      <div class="vs-center-wrap arrow-top">
+        <div class="vs-center">
+
+          <div class="fogged">
+            <h1 class="design-five--title">хотите увидеть все</h1>
+            <h2 class="design-five--subtitle">наши работы?</h2>
+            <h3 class="design-five--thirdtitle">скачайте полный каталог прямо сейчас</h3>
+            <a href="#" class="btn btn-red">скачать каталог наших работ</a>
+            <h5 class="design-five--description"><span>остались вопросы? задайте их менеджеру</span></h5>
+          </div><!-- /.fogged -->
+
+        </div><!-- vs-center -->
+      </div><!-- vs-center-wrap -->
+    </div><!-- mainview -->
+
+  </div><!-- mainbag -->
+
+  <div class="modalbg modal-ancient-offer">
+    <div class="modal-c">
+      <h5>получите полный каталог</h5>
+      <h6>заполните поля ниже и мы вышлем вам полный каталог старинного кирпича</h6>
+      <form method="post">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="phone" data-inputmask="'alias': 'phone'" placeholder="номер телефона">
+        <button class="btn btn-red">получить полный каталог</button>
+      </form>
+      <span class="form-sended">ваша заяка принята<span>мы свяжемся с вами в ближайшее время</span></span>
+      <span class="close"></span>
+    </div><!-- /.modal-c -->
+  </div><!-- /.modalbg -->
+
+  <div class="modalbg modal-ancient-sms">
+    <div class="modal-c">
+      <h6>введите данные в форму ниже мы свяжемся с вами и обсудим детли вашего проекта </h6>
+      <form method="post">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="phone" data-inputmask="'alias': 'phone'" placeholder="номер телефона">
+        <button class="btn btn-red">получить полный каталог</button>
+      </form>
+      <span class="form-sended">ваша заяка принята<span>мы свяжемся с вами в ближайшее время</span></span>
+      <span class="close"></span>
+    </div><!-- /.modal-c -->
+  </div><!-- /.modalbg -->
+
+  <div class="modalbg">
+    <div class="modal-c">
+      <h5>получите полный каталог</h5>
+      <h6>заполните поля ниже и мы вышлем вам полный каталог старинного кирпича</h6>
+      <form action="">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="phone" data-inputmask="'alias': 'phone'" placeholder="номер телефона">
+        <button class="btn btn-red">получить полный каталог</button>
+      </form>
+      <span class="close"></span>
+    </div><!-- /.modal-c -->
+  </div><!-- /.modalbg -->
+
+
+  <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+  <script type="text/javascript" src="js/scripts.js"></script>
+
+</body>
+</html>
