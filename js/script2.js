2ad0fbf7ad8b0663b5b02be7c49d93be5abfab0f
hsrazna <anzar.terek@gmail.com>
2016-11-29 10:06:54 +0300

    dsfd

--- js/script2.js
+++ js/script2.js
@@ -1,61 +1,12 @@
-// $(".design--item .az-manager .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modalbg-manager").css("display","flex");
-// });
-// $
-// $(".ancient-offer .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".ancient-fourth .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".ancient-nine .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".weoffer .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".home-third .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".design--item .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".design-first .btn-red").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".design-second .btn-red").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".design-third .btn-red").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".design-fourth .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".design-five .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".home-eight .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-offer").css("display","flex");
-// });
-// $(".ancient-six .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-sms").css("display","flex");
-// });
-// $(".home-five .btn").on("click",function(e){
-// 	e.preventDefault();
-// 	$(".modal-ancient-sms").css("display","flex");
-// });
+$(document).ready(function(){
+	// alert(1);
+	$('.az-file2').click(function(){
+		$(this).blur();
+		$('.az-file').trigger('click');
+		return false;
+	});
+	$('.az-file').change(function(){
+	  $(this).next('.az-file2').val($(this).val().substring($(this).val().lastIndexOf('\\')+1,$(this).val().length));
+	  // $(this).siblings('input[type="hidden"]').attr('value', $(this).val().substring($(this).val().lastIndexOf('\\')+1,$(this).val().length));
+	});
+});
